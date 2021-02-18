fa4d79bef4d33dd6c0b841d3c08520b2d88cb239
Mai Hameed <maihameed@hotmail.com>
2021-02-18 17:11:21 +0000

    Working and completed lab2

--- lab2/initiator.py
+++ lab2/initiator.py
@@ -2,16 +2,45 @@ from des import DesKey
 import socket
 
 HOST = 'localhost'
-PORT = 9060
+PORT = 9080
 BUFFER = 1024
 
 ida = b'INITIATOR A'
-key = DesKey(b'NETWORK SECURITY')
+keym = DesKey(b'NETWORK SECURITY')
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST,PORT))
+    
+    # Sending cleartext of message 1
     s.sendall(ida)
-    print('Sent the following data to the responder: ', ida)
-    data = s.recv(BUFFER)
+    print('=== Sent ===')
+    print(ida)
 
-print('Received ',repr(data))
+    # Received ciphertext of message 2
+    e_keys = s.recv(BUFFER)
+    e_ida = s.recv(BUFFER)
+    e_idb = s.recv(BUFFER)
+    print('=== Received Encrypted Messages ===')
+    print(e_keys)
+    print(e_ida)
+    print(e_idb)
+
+    # Decrypted message 2
+    keys = keym.decrypt(e_keys, padding=True)
+    ida = keym.decrypt(e_ida, padding=True)
+    idb = keym.decrypt(e_idb, padding=True)
+    print('=== Decrypted Messages Into ===')
+    print(keys)
+    print(ida)
+    print(idb)
+
+    # Sending ciphertext of message 3
+    keys = DesKey(keys)
+    e_idb = keys.encrypt(idb, padding=True)
+    s.sendall(e_idb)
+    print('=== Sent ===')
+    print(e_idb)
+    
+    print('=== Disconnecting ===')
+    s.close()
+    exit()
--- lab2/responder.py
+++ lab2/responder.py
@@ -1,31 +1,68 @@
 from des import DesKey
 import socket
+from time import sleep
 
 HOST = 'localhost'
-PORT = 9060
+PORT = 9080
 BUFFER = 1024
 
+# Plaintext messages
 ida = b'INITIATOR A'
 idb = b'RESPONDER B'
-key = DesKey(b'NETWORK SECURITY')
+keym = DesKey(b'NETWORK SECURITY')
+keys = b'RYERSON ' # Key must be 8 bytes long
 
 # Encrypted plaintext
-e_idb = key.encrypt(idb, padding=True)
+e_keys = keym.encrypt(keys, padding=True)
+e_ida = keym.encrypt(ida, padding=True)
+e_idb = keym.encrypt(idb, padding=True)
 
+# Opening a TCP connection
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # Binds to the socket
     s.bind((HOST,PORT))
     s.listen()
+    # continuously accepts incoming clients
     while 1:
         conn, addr = s.accept()
         with conn:
-                print(addr,' is currently connected')
-                while 1:
+            print(addr,' is currently connected')
+            while 1:
+                # waits for incoming data from the connected client
+                data = conn.recv(BUFFER)
+                if not data:
+                    print(addr,' disconnected')
+                    break
+                # checks to see if the client is ID-a
+                elif (data == ida):
+                    # Received message 1
+                    print('=== Received ===')
+                    print(data)
+
+                    # Sending ciphertext of message 2
+                    conn.sendall(e_keys)
+                    sleep(0.01) # Adding delays to prevent mismatch of message sending
+                    conn.sendall(e_ida)
+                    sleep(0.01)
+                    conn.sendall(e_idb)
+                    print('=== Sent ===')
+                    print(e_keys)
+                    print(e_ida)
+                    print(e_idb)
+
+                    # Received ciphertext of message 3
                     data = conn.recv(BUFFER)
-                    if not data:
-                        print(addr,' disconnected')
-                        break
-                    print('Received: ', data)
-                    if (data == ida):
-                        print('Received: ', data)
-                        print('Established connection with initiator')
-                        
+                    print('=== Received ===')
+                    print(data)
+
+                    # Decrypted message 3
+                    keys = DesKey(keys)
+                    data = keys.decrypt(data, padding=True)
+                    print('=== Decrypted ===')
+                    print(data)
+
+                    print('=== Disconnecting ===')
+                    s.close()
+                    exit()
+                else:
+                    break
